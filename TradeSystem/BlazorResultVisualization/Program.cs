10841a7592cbbac48dfdddedc1ab71c6869eaa29
samantonovich@gmail.com <samantonovich@gmail.com>
2021-08-26 12:34:14 +0300

    Set center pos for loading div

--- TradeSystem/BlazorResultVisualization/Program.cs
+++ TradeSystem/BlazorResultVisualization/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -17,7 +18,14 @@ namespace BlazorResultVisualization
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDc1OTA4QDMxMzkyZTMyMmUzMERaSERFMzhJdkRvVlN2cVlHT0t0WFVVczQvQmxvNU9PNzRLM3VqYjlEaWc9");
+
+            //builder.Services.AddHttpClient<IDataService, RESTDataService>
+            //    (spds => spds.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
