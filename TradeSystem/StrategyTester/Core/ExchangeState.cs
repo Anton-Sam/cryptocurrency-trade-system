5863e41a3d309d5d889062836a949a9b94817194
samantonovich@gmail.com <samantonovich@gmail.com>
2021-08-26 10:55:44 +0300

    Fix using in lib

--- TradeSystem/StrategyTester/Core/ExchangeState.cs
+++ TradeSystem/StrategyTester/Core/ExchangeState.cs
@@ -1,11 +1,6 @@
-﻿using Binance.Net;
+﻿using StrategyTester.Models;
 using StrategyTester.Models.Test;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using StrategyTester.Extensions;
 
 namespace StrategyTester.Core
 {
--- TradeSystem/StrategyTester/Models/BalanceChange.cs
+++ TradeSystem/StrategyTester/Models/BalanceChange.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StrategyTester.Models.Test
+namespace StrategyTester.Models
 {
     public class BalanceChange
     {
