d14ebbafd27f257448b9915f63b30599f5370d5b
Anton <samantonovich@gmail.com>
2021-08-30 22:25:34 +0300

    Add dropdown lists

---
+++ TradeSystem/BlazorResultVisualization/Pages/Settings/SymbolsDropDownList.cs
@@ -0,0 +1,24 @@
+﻿using Binance.Net;
+using BlazorResultVisualization.DataServices;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorResultVisualization.Pages.Settings
+{
+    public partial class SymbolsDropDownList
+    {
+        [Inject]
+        public SettingsDataService SettingsDataService { get; set; }
+
+        private BinanceClient _client=new BinanceClient();
+        private string _selectedSymbol;
+        //private IEnumerable<string> _symbols=_client.Spot.;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            
+        }
+    }
+}
