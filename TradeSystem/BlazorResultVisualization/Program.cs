4b01357f88a20f138cd53d96db2c85468790021a
samantonovich@gmail.com <samantonovich@gmail.com>
2021-08-31 17:55:03 +0300

    Add configurestrategies

--- TradeSystem/BlazorResultVisualization/Program.cs
+++ TradeSystem/BlazorResultVisualization/Program.cs
@@ -1,8 +1,12 @@
+using Binance.Net;
 using BlazorResultVisualization.DataServices;
+using BlazorResultVisualization.Strategies;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using StrategyTester.Models;
 using Syncfusion.Blazor;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -21,11 +25,20 @@ namespace BlazorResultVisualization
             //    (spds => spds.BaseAddress = new Uri(builder.Configuration["api_base_url"]));
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddSingleton(new SettingsDataService());
+            builder.Services.AddSingleton(ConfigureStrategies());
 
             builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
+        }
+
+        private static SettingsDataService ConfigureStrategies()
+        {
+            var client = new BinanceClient();
+            var settings = new SettingsDataService();
+            settings.StrategiesDict = new Dictionary<string, BaseStrategy>();
+            settings.StrategiesDict.Add("TwoEma", new TwoEmaStrategy());
+            return settings;
         }
     }
 }
