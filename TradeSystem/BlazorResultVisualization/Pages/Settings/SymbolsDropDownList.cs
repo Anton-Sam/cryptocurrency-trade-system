c21c13230b7cd5bbbca330761093c978a2118263
samantonovich@gmail.com <samantonovich@gmail.com>
2021-08-31 17:55:25 +0300

    Fix settings view

--- TradeSystem/BlazorResultVisualization/Pages/Settings/SymbolsDropDownList.cs
+++ TradeSystem/BlazorResultVisualization/Pages/Settings/SymbolsDropDownList.cs
@@ -1,7 +1,9 @@
 ﻿using Binance.Net;
 using BlazorResultVisualization.DataServices;
 using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.DropDowns;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorResultVisualization.Pages.Settings
@@ -11,14 +13,21 @@ namespace BlazorResultVisualization.Pages.Settings
         [Inject]
         public SettingsDataService SettingsDataService { get; set; }
 
-        private BinanceClient _client=new BinanceClient();
+        private BinanceClient _client;
         private string _selectedSymbol;
-        //private IEnumerable<string> _symbols=_client.Spot.;
+        private IEnumerable<string> _symbols;
 
         protected override async Task OnInitializedAsync()
         {
+            //_client = new BinanceClient();
+            //_symbols = (await _client.Spot.System.GetExchangeInfoAsync()).Data.Symbols.Select(s => s.Name);
+            _symbols = new List<string> { "btcusdt", "ltcusdt" };
             await base.OnInitializedAsync();
-            
         }
+
+        //private void OnValueChange(ChangeEventArgs<string, string> args)
+        //{
+        //    SettingsDataService.Symbol = _selectedSymbol;
+        //}
     }
 }
