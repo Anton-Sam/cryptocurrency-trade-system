a1b842d70c6770fd88fa229edf81dffa71af1204
Anton <samantonovich@gmail.com>
2021-09-05 23:25:26 +0300

    Add mapper di

--- TradeSystem/BlazorTestingsSystem/Startup.cs
+++ TradeSystem/BlazorTestingsSystem/Startup.cs
@@ -12,17 +12,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace BlazorTestingsSystem
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -32,10 +35,20 @@ namespace BlazorTestingsSystem
             {
                 e.MaximumReceiveMessageSize = 102400000;
             });
+
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddServerSideBlazor().AddCircuitOptions(o =>
+            {
+                if (_env.IsDevelopment()) //only add details when debugging
+                {
+                    o.DetailedErrors = true;
+                }
+            });
             services.AddSyncfusionBlazor();
-            services.AddSingleton(ConfigureStrategies());
+            //services.AddSingleton(ConfigureStrategies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,13 +77,13 @@ namespace BlazorTestingsSystem
             });
         }
 
-        private static SettingsDataService ConfigureStrategies()
-        {
-            //var client = new BinanceClient();
-            var settings = new SettingsDataService();
-            settings.StrategiesDict = new Dictionary<string, BaseStrategy>();
-            settings.StrategiesDict.Add("TwoEma", new TwoEmaStrategy());
-            return settings;
-        }
+        //private static SettingsDataService ConfigureStrategies()
+        //{
+        //    //var client = new BinanceClient();
+        //    var settings = new SettingsDataService();
+        //    settings.StrategiesDict = new Dictionary<string, BaseStrategy>();
+        //    settings.StrategiesDict.Add("TwoEma", new TwoEmaStrategy());
+        //    return settings;
+        //}
     }
 }
