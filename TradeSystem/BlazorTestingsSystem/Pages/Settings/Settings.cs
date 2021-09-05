a2936d8d19933e10017355b083af6c0fc8ab041d
Anton <samantonovich@gmail.com>
2021-09-05 23:24:33 +0300

    Fix settings pages

--- TradeSystem/BlazorTestingsSystem/Pages/Settings/Settings.cs
+++ TradeSystem/BlazorTestingsSystem/Pages/Settings/Settings.cs
@@ -1,48 +1,97 @@
-﻿using BlazorTestingsSystem.DataServices;
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿//using BlazorTestingsSystem.DataServices;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components.Forms;
+//using Syncfusion.Blazor.Inputs;
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Linq;
+//using System.Reflection;
+//using System.Threading.Tasks;
 
-namespace BlazorTestingsSystem.Pages.Settings
-{
-    public partial class Settings
-    {
+//namespace BlazorTestingsSystem.Pages.Settings
+//{
+//    public enum CandleInterval
+//    {
+//        [Display(Name = "OneMinute")]
+//        OneMinute,
+//        [Display(Name = "ThreeMinute")]
+//        ThreeMinutes,
+//        [Display(Name = "FiveMinute")]
+//        FiveMinutes,
+//        [Display(Name = "FifteenMinutes")]
+//        FifteenMinutes,
+//        [Display(Name = "ThirtyMinutes")]
+//        ThirtyMinutes,
+//        [Display(Name = "OneHour")]
+//        OneHour,
+//        [Display(Name = "TwoHour")]
+//        TwoHour,
+//        [Display(Name = "FourHour")]
+//        FourHour,
+//        [Display(Name = "SixHour")]
+//        SixHour,
+//        [Display(Name = "EightHour")]
+//        EightHour,
+//        [Display(Name = "TwelveHour")]
+//        TwelveHour,
+//        [Display(Name = "OneDay")]
+//        OneDay,
+//        [Display(Name = "ThreeDay")]
+//        ThreeDay,
+//        [Display(Name = "OneWeek")]
+//        OneWeek,
+//        [Display(Name = "OneMonth")]
+//        OneMonth
+//    }
+//    public partial class Settings
+//    {
+//        [Inject]
+//        public SettingsDataService SettingsDataService { get; set; }
 
-        private Annotation annotation = new Annotation() { };
-        private void HandleValidSubmit()
-        {
-            Console.WriteLine("OnValidSubmit");
+//        private void click()
+//        {
+//            cssClass = "cssclasschag";
+//        }
 
-        }
-        public class Annotation
-        {
-            [Required]
-            public string TextValue { get; set; }
-            [Required]
-            public string MaskValue { get; set; }
-        }
+//        private CandleInterval? _interval;
+//        private SettingsModel model;
+//        private EditContext editContext;
+//        private string cssClass { get; set; }
+//        protected override void OnInitialized()
+//        {
+//            model = new SettingsModel();
+//            editContext = new EditContext(model);
+//        }
+//        public class SettingsModel
+//        {
+//            [Required]
+//            public string TestProperty { get; set; }
+//            [Required]
+//            public CandleInterval? Interval { get; set; }
+//        }
+//        public void TestPropertyBlurEvent(FocusOutEventArgs args)
+//        {
+//            if (!editContext.Validate())
+//            {
+//                cssClass = "e-error";
+//            }
+//            else
+//            {
+//                cssClass = "e-success";
+//            }
+//            StateHasChanged();
+//        }
+//        protected override async Task OnInitializedAsync()
+//        {
+//            await base.OnInitializedAsync();
 
+//        }
+//        public void Dispose()
+//        {
 
-        [Inject]
-        public SettingsDataService SettingsDataService { get; set; }
-
-        [Required]
-        public string Value { get; set; }
-
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-
-        }
-        public void Dispose()
-        {
-
-        }
-    }
+//        }
+//    }
 
     
-}
+//}
