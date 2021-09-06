55fa2c11e8ccdc49d888c0fc9db813a706a2eeed
samantonovich@gmail.com <samantonovich@gmail.com>
2021-09-06 17:53:40 +0300

    Add mappers

---
+++ TradeSystem/BlazorTestingsSystem/Profiles/BalancesProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BlazorTestingsSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTestingsSystem.Profiles
+{
+    public class BalancesProfile : Profile
+    {
+        public BalancesProfile() =>
+            CreateMap<StrategyTester.Models.BalanceChange, Balance>()
+            .ForMember(scr => scr.Amount, opt => opt.MapFrom(value => value.Balance));
+    }
+}
---
+++ TradeSystem/BlazorTestingsSystem/Profiles/OrdersProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BlazorTestingsSystem.Data;
+
+namespace BlazorTestingsSystem.Profiles
+{
+    public class OrdersProfile : Profile
+    {
+        public OrdersProfile() =>
+            CreateMap<StrategyTester.Models.Order, Order>();
+    }
+}
