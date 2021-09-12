357805ee2d6dfd78fee16f0a1c98ebeaeae4d1e6
Anton <samantonovich@gmail.com>
2021-09-12 23:12:25 +0300

    Transfer virtual storage to another namespace

---
+++ TradeSystem/StrategyTester/Models/Test/ExchangeState.cs
@@ -0,0 +1,20 @@
+﻿using StrategyTester.Models;
+using StrategyTester.Models.Test;
+using System.Collections.Generic;
+
+namespace StrategyTester.Test
+{
+    class ExchangeState
+    {
+        internal int OrderId { get; set; }
+        internal TestingSettings TestingSettings { get; set; }
+        internal List<Order> OpenOrders { get; set; } = new List<Order>();
+        internal List<Order> OrdersHistory { get; set; } = new List<Order>();
+        internal List<Candle> HistoryCandles { get; set; } = new List<Candle>();
+        internal Balance BaseAssetBalance { get; set; } = new Balance();
+        internal Balance QuoteAssetBalance { get; set; } = new Balance();
+        internal SymbolInfo SymbolInfo { get; set; } = new SymbolInfo();
+        internal Candle LastCandle { get; set; }
+        //internal TradeSettings TradeSettings { get; set; }
+    }
+}
--- TradeSystem/StrategyTester/TradeClient.cs
+++ TradeSystem/StrategyTester/TradeClient.cs
@@ -1,37 +1,37 @@
 ﻿using Microsoft.Extensions.Logging;
 using StrategyTester.DataProviders;
-using StrategyTester.Models.Common;
+using StrategyTester.Models.Interfaces;
 using StrategyTester.Models.Test;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace StrategyTester
 {
     public class TradeClient
     {
+        private readonly ILogger _logger;
         public TradeClient() { }
-        public TradeClient(ILogger logger) {
-            logger.LogInformation("Trade client created");
+        public TradeClient(ILogger logger)
+        {
+            _logger = logger;
+            _logger?.LogInformation("Trade client created");
         }
 
-        public TestingResult StartTest(BaseStrategy strategy,TestingSettings settings)
+        public TestingResult StartTest(IStrategy strategy, TestingSettings settings)
         {
             var paperDataProvider = new PaperDataProvider();
+            paperDataProvider.Logger = _logger;
             strategy.DataProvider = paperDataProvider;
 
             paperDataProvider.OnCandleClosed += strategy.OnCandleClosed;
             paperDataProvider.OnOrderStatusChanged += strategy.OnOrderStatusChanged;
 
-            var result= paperDataProvider.StartTrading(settings);
+            var result = paperDataProvider.StartTrading(settings);
             return result;
         }
 
-        public async Task<TestingResult> StartTestAsync(BaseStrategy strategy,TestingSettings settings)
+        public async Task<TestingResult> StartTestAsync(IStrategy strategy, TestingSettings settings)
         {
-            return await Task.Run(() => StartTest(strategy,settings));
+            return await Task.Run(() => StartTest(strategy, settings));
         }
     }
 }
