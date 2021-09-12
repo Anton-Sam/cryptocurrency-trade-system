57034d69563a146c2ca82f60ec803db0be2846fd
Anton <samantonovich@gmail.com>
2021-09-12 23:10:33 +0300

    Swith abstracts on interfaces

---
+++ TradeSystem/StrategyTester/Interfaces/IDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using StrategyTester.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyTester.Models.Interfaces
+{
+    public interface IDataProvider
+    {
+        ILogger Logger { get; set; }
+        Order PlaceOrder(string clientOrderId, OrderSide side, OrderType orderType, decimal quantity, decimal? price = null);
+        Order CancelOrder(string orderId, string clientOrderId);
+        IEnumerable<Order> CancelAllOpenOrders();
+        IEnumerable<Order> GetOpenOrders();
+        Balance GetBaseAssetBalance();
+        Balance GetQuoteAssetBalance();
+        IEnumerable<Balance> GetAllBalances();
+        IEnumerable<Candle> GetLastCandles(int count);
+
+        event Action<Order> OnOrderStatusChanged;
+        event Action<Candle> OnCandleClosed;
+    }
+}
---
+++ TradeSystem/StrategyTester/Interfaces/IStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace StrategyTester.Models.Interfaces
+{
+    public interface IStrategy
+    {
+        IDataProvider DataProvider { get; set; }
+        void OnOrderStatusChanged(Order order);
+        void OnCandleClosed(Candle candle);
+    }
+}
